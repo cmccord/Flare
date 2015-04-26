10095ff9f4cf258e8b34f9a7dd632e2c1bcec04f
kwdougla <kwdougla@princeton.edu>
2015-04-25 21:26:21 -0400

    newuser

--- app/src/main/java/sunglass/com/loco/newUserActivity.java
+++ app/src/main/java/sunglass/com/loco/newUserActivity.java
@@ -2,22 +2,40 @@ package sunglass.com.loco;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class newUserActivity extends Activity {
+
+    private String mImei;
+    private Firebase ref;
+    private EditText mEmail;
+    private EditText mDisplayName;
+    private EditText mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user);
+
+        Firebase.setAndroidContext(this);
 
         Button mBackButton = (Button) findViewById(R.id.back_butt);
         mBackButton.setOnClickListener(new View.OnClickListener() {
@@ -38,13 +56,59 @@ public class newUserActivity extends Activity {
                         .setMessage("All Set?")
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
+
+                                mEmail = (EditText)findViewById(R.id.editEmail);
+                                mDisplayName = (EditText)findViewById(R.id.editDisplayName);
+                                mPassword = (EditText)findViewById(R.id.editPassword);
+
+                                ref = new Firebase("https://loco-android.firebaseio.com");
+                                ref.createUser(mEmail.getText().toString(), mPassword.getText().toString(), new Firebase.ValueResultHandler<Map<String, Object>>() {
+                                    @Override
+                                    public void onSuccess(Map<String, Object> result) {
+
+                                        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+                                        mImei = telephonyManager.getDeviceId();
+
+                                        if (ref != null) {
+                                            Map user = new HashMap<>();
+                                            Map dets = new HashMap<>();
+                                            dets.put("pos", "0.00000000,-20.00000000");
+                                            dets.put("name", mDisplayName.getText().toString());
+                                            dets.put("timestamp", System.currentTimeMillis());
+                                            dets.put("time_created", System.currentTimeMillis());
+                                            user.put(mImei, dets);
+
+                                            ref.child("users").updateChildren(user);
+                                        }
+
+                                        Toast.makeText(getApplicationContext(), "Success! uid: " + result.get("uid"), Toast.LENGTH_SHORT).show();
+
+                                        ref.authWithPassword(mEmail.getText().toString(), mPassword.getText().toString(), new Firebase.AuthResultHandler() {
+                                            @Override
+                                            public void onAuthenticated(AuthData authData) {
+                                                Toast.makeText(getApplicationContext(), "User ID: " + authData.getUid() + ", Password: " + authData.getProvider(), Toast.LENGTH_SHORT).show();
+                                            }
+
+                                            @Override
+                                            public void onAuthenticationError(FirebaseError firebaseError) {
+                                                Toast.makeText(getApplicationContext(), "Log In Failed", Toast.LENGTH_SHORT).show();
+                                            }
+                                        });
+                                    }
+
+                                    @Override
+                                    public void onError(FirebaseError firebaseError) {
+                                        Toast.makeText(getApplicationContext(), "Create New User Failed", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
                                 Intent i = new Intent(newUserActivity.this, MapsActivity.class);
                                 startActivity(i);
                             }
                         })
                         .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
-                                // Do nothing
+                                // Do nothing.
                             }
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
@@ -54,6 +118,7 @@ public class newUserActivity extends Activity {
 //                startActivity(i);
             }
         });
+
     }
 
 }
