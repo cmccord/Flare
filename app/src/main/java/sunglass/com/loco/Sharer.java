68b7b555ead875e5f952b8c3fbac9d9f2d54d7d1
cmccord <cgm708@gmail.com>
2015-05-07 21:35:00 -0400

    Sliders improved

--- app/src/main/java/sunglass/com/loco/Sharer.java
+++ app/src/main/java/sunglass/com/loco/Sharer.java
@@ -16,10 +16,11 @@ public class Sharer{
     private int duration = 60; // in minutes
     private int frequency = 60; // in seconds
     private Activity mParent;
+    private SeekBar frequencyBar;
     public Sharer(final Activity parent) {
         mParent = parent;
         SeekBar durationBar = (SeekBar) parent.findViewById(R.id.seekBarDuration);
-        SeekBar frequencyBar = (SeekBar) parent.findViewById(R.id.seekBarFrequency);
+        frequencyBar = (SeekBar) parent.findViewById(R.id.seekBarFrequency);
         durationBar.setOnSeekBarChangeListener(new durationBarListener());
         frequencyBar.setOnSeekBarChangeListener(new frequencyBarListener());
 
@@ -60,6 +61,14 @@ public class Sharer{
             TextView durationSetting = (TextView) mParent.findViewById(R.id.durationSetting);
             duration = progress + 1;
             durationSetting.setText(duration + " m");
+            int max;
+            if(progress < 60)
+                max = progress + 55;
+            else if(progress < 119)
+                max = 114;
+            else
+                max = 115;
+            frequencyBar.setMax(max);
         }
         @Override
         public void onStartTrackingTouch(SeekBar seekBar){}
@@ -71,17 +80,17 @@ public class Sharer{
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             TextView frequencySetting = (TextView) mParent.findViewById(R.id.frequencySetting);
-            if(progress < 40) {
-                frequency = progress + 20;
+            if(progress < 55) {
+                frequency = progress + 5;
                 frequencySetting.setText(frequency + " s");
             }
-            else if(progress < 99) {
-                frequency = (progress - 39) * 60;
-                frequencySetting.setText(progress - 39 + " m");
+            else if(progress < 114) {
+                frequency = (progress - 54) * 60;
+                frequencySetting.setText(progress - 54 + " m");
             }
             else {
-                frequency = (progress - 98) * 3600;
-                frequencySetting.setText(progress - 98 + " hr");
+                frequency = (progress - 113) * 3600;
+                frequencySetting.setText(progress - 113 + " hr");
             }
         }
         @Override
